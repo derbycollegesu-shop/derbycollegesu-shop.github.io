3535fc413710284210c35b3bf82e13a6617836fd
markrugman <mark@marksveryown.com>
2017-06-20 10:12:42 +0100

    Litte changes

---
+++ templates/js/routes.js
@@ -0,0 +1,132 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController.homePage', {
+    url: '/home',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/homePage.html',
+        controller: 'homePageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.shop', {
+    url: '/shop',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/shop.html',
+        controller: 'shopCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.adminPage', {
+    url: '/admin',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/adminPage.html',
+        controller: 'adminPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/tabs',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/login',
+	params: {
+		prevState: "",
+		prevParams: ""		
+},
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('tabsController.ordersPage', {
+    url: '/orderspage',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/ordersPage.html',
+        controller: 'ordersPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.orderPage', {
+    url: '/orderpage',
+	params: {
+		id: "4058"		
+},
+    views: {
+      'tab3': {
+        templateUrl: 'templates/orderPage.html',
+        controller: 'orderPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.productSections', {
+    url: '/sections',
+	params: {
+		category: ""		
+},
+    views: {
+      'tab2': {
+        templateUrl: 'templates/productSections.html',
+        controller: 'productSectionsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.products', {
+    url: '/products',
+	params: {
+		section: ""		
+},
+    views: {
+      'tab2': {
+        templateUrl: 'templates/products.html',
+        controller: 'productsCtrl'
+      }
+    }
+  })
+
+  .state('myAccount', {
+    url: '/myaccount',
+    templateUrl: 'templates/myAccount.html',
+    controller: 'myAccountCtrl'
+  })
+
+  .state('tabsController.notifications', {
+    url: '/notifications',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/notifications.html',
+        controller: 'notificationsCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+
+});
---
+++ templates/js/services.js
@@ -0,0 +1,28 @@
+angular.module('app.services', [])
+
+.run(['$rootScope', function($rootScope){
+    $rootScope.user = [];
+    $rootScope.user.name = "User Name";
+    $rootScope.user.email = "example@example.com";
+    $rootScope.user.gravImg = "https://www.gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8.jpg?s=100";
+}])
+
+.factory('shopFactory', [function(){
+    var shopCategories = [];
+    var shopSections = [];
+    var shopProducts = [];
+
+    return {
+        categories: shopCategories,
+        sections: shopSections,
+        products: shopProducts
+    };
+}])
+
+
+.factory('dataService', [function(){
+    var data = [];
+    
+    
+    return {data: data, doit: function(x){console.log(x);}};
+}]);
