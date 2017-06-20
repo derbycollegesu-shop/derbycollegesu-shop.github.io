e55ce65adc58354cd4b939cbe1ec3df4d619790d
markrugman <mark@marksveryown.com>
2017-06-20 11:14:55 +0100

    Trying to grab url params

--- js/controllers.js
+++ js/controllers.js
@@ -65,8 +65,47 @@ function ($scope, $stateParams, shopFactory) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-    alert(location.search);
+    //alert(location.search);
 
+function ptq(q)
+{
+/* parse the query */
+/* semicolons are nonstandard but we accept them */
+var x = q.replace(/;/g, '&').split('&'), i, name, t;
+/* q changes from string version of query to object */
+for (q={}, i=0; i<x.length; i++)
+{
+t = x[i].split('=', 2);
+name = unescape(t[0]);
+if (!q[name])
+q[name] = [];
+if (t.length > 1)
+{
+q[name][q[name].length] = unescape(t[1]);
+}
+/* next two lines are nonstandard */
+else
+q[name][q[name].length] = true;
+}
+return q;
+}
+
+function param() {
+return ptq(location.search.substring(1).replace(/\+/g, ' '));
+}
+
+q = param();
+
+for (var k in q)
+{
+  
+{
+
+for (i=0; i<q[k].length; i++)
+alert(k + ' ' + q[k][i]);
+}
+
+}
 
 }])
       
