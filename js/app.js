beec81645d5f61ac92212e1d33b8fac6419d19ba
markrugman <mark@marksveryown.com>
2017-06-23 15:08:14 +0100

    CSS Updates

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services','gravatar','firebase','firebaseConfig','app.tabs','authModule','app.header',])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services','gravatar','firebase','firebaseConfig','app.tabs','authModule','app.header','uiGmapgoogle-maps','googlemaps.init',])
 
 .config(function($ionicConfigProvider, $sceDelegateProvider){
 
